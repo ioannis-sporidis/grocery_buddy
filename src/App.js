68589f583178599c5a25c57ce28dc4580a1ae50d
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-08-07 00:30:28 -0400

    Setup static items

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
+import { useState } from 'react';
+import List from './components/List';
+import Alert from './components/Alert';
+
 function App() {
-  return <div className='App'></div>;
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: '',
+    type: '',
+  });
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('Submitted');
+  };
+
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery buddy</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs'
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      <div className='grocery-container'>
+        <List />
+        <button className='clear-btn'>Clear Items</button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
